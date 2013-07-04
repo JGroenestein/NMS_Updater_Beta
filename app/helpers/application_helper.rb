d3ceb2fe35ef55032b2e6cbbd6d8ca720427a302
JGroenestein <j.groenestein@gmail.com>
2013-07-05 06:36:37 +1000

    Added skin picker

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,15 @@ module ApplicationHelper
     path ? 'active open' : ''
   end
 
+  def skin_method
+    @user = current_user
+    case @user.skin
+      when "#222A2D"
+        "class=skin-1"
+      when "#C6487E"
+        "class=skin-2"
+      when "#D0D0D0"
+        "class=skin-3"
+    end
+  end
 end
