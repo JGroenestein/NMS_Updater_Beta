d8f36a73a1133ee7f16f1d8eb60b14bbcc70f42b
JGroenestein <j.groenestein@gmail.com>
2013-07-05 00:06:05 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NMSUpdaterBeta::Application.config.secret_token = 'd897b21bae1c214e4eed0cce9df4c7c6d00b7ded96084e6ddbfb1aa26150728a7dde9b1ee03db48445522d33eb7448959cda9b0df5e61093c284023948fba353'
