cf08af5716742085114375b147ca1ce77a055096
JGroenestein <j.groenestein@gmail.com>
2013-07-10 23:23:10 +1000

    Ready for production

--- db/schema.rb
+++ db/schema.rb
@@ -11,43 +11,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130705172324) do
-
-  create_table "nms_updates", :force => true do |t|
-    t.string   "service_type"
-    t.string   "basestation"
-    t.string   "direction"
-    t.string   "device_make"
-    t.string   "device_model"
-    t.string   "bigair_service_number"
-    t.string   "partner"
-    t.string   "business_name"
-    t.string   "address"
-    t.string   "device_ip"
-    t.string   "added_by"
-    t.string   "completed_by"
-    t.datetime "created_at",            :null => false
-    t.datetime "updated_at",            :null => false
-  end
+ActiveRecord::Schema.define(:version => 20130704162751) do
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
+    t.string   "email",               :default => "", :null => false
+    t.string   "encrypted_password",  :default => "", :null => false
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",       :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                          :null => false
+    t.datetime "updated_at",                          :null => false
     t.string   "username"
-    t.string   "skin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
